51b169c934c216d54d296793f22e20738ffaec87
FathiGuemri <zlatan.guemri@gmail.com>
2019-12-26 02:55:46 +0100

    database uri

--- db/mongoos.js
+++ db/mongoos.js
@@ -1,7 +1,7 @@
 const mongoose  =  require('mongoose');
 const mongoPassword = 'Est-367426'
 
-let uri ="mongodb://03afe9f04ddd62ddeef5de17e68a439d:" + encodeURIComponent(mongoPassword) + "@mongodb:27017/03afe9f04ddd62ddeef5de17e68a439d";
+let uri ="mongodb://ugkr7z7fsvcq8qifu4ea:rlfVoXlHCP4JtNfmdUJQ@brpou01tbkrpeth-mongodb.services.clever-cloud.com:27017/brpou01tbkrpeth";
  mongoose.Promise = global.Promise
  mongoose.connect(uri , {useNewUrlParser: true}).then(() => {
         console.log('conected to mangodb succesfully :)')
